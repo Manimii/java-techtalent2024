f61f29d4042450fce229c1fc1e46319550570402
Manel Nin <mninminet@gmail.com>
2024-02-02 10:27:56 +0100

    Actividad 2

---
+++ Proyecto Java/src/TA2/ComentarioVariasLineas.java
@@ -0,0 +1,17 @@
+package TA2;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*
+		 * System.out.println("Hola"); System.out.println("Adios");
+		 * System.out.println("Cucu");
+		 */
+		for (int i = 0; i < 6; /* Ahora viene el paso */ i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("FIN DEL PROGRAMA");
+
+	}
+
+}
