e594f40c053f060ba8531ffa90500df7edadeb31
Manel Nin <mninminet@gmail.com>
2024-02-05 09:55:03 +0100

    Creación package tests y operadores.java

---
+++ C2/src/tests/operadores.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class operadores {
+
+	public static void main(String[] args) {
+		int op = 10;
+		System.out.println(op);
+		System.out.println(op++);
+		System.out.println(++op);
+	}
+
+}
