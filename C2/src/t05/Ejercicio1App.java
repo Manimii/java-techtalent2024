07492878714dfb4b73d1a09fb7f84d08a0c794fc
Manel Nin <mninminet@gmail.com>
2024-02-06 13:58:41 +0100

    Create Ejercicio1App.java

---
+++ C2/src/t05/Ejercicio1App.java
@@ -0,0 +1,17 @@
+package t05;
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		int x = 10, y = 20;
+		
+		if (x > y) {
+			
+		} else if (x == y) {
+			
+		} else {
+			
+		}
+	}
+
+}
