89527aff5f603dc8e14d03557ae9a5ba18868919
Manel Nin <mninminet@gmail.com>
2024-02-14 10:47:36 +0100

    Update MetodosApp.java

--- C2/src/tests/MetodosApp.java
+++ C2/src/tests/MetodosApp.java
@@ -6,20 +6,19 @@ public class MetodosApp {
 
 		int num1 = 10, num2 = 5;
 		int suma = sumaNumeros(num1, num2);
-		
+
 		if (mayorZero(suma)) {
 			System.out.println("Resultado positivo");
 		} else {
 			System.out.println("Resultado negativo");
 		}
-		
-		
+
 	}
 
 	public static int sumaNumeros(int a, int b) {
 		return a + b;
 	}
-	
+
 	public static boolean mayorZero(int a) {
 		return a >= 0;
 	}
