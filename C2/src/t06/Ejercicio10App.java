b55a578250858960dc08718e5039290bce0e3d4e
Manel Nin <mninminet@gmail.com>
2024-02-16 12:08:35 +0100

    Update Ejercicio10App.java

--- C2/src/t06/Ejercicio10App.java
+++ C2/src/t06/Ejercicio10App.java
@@ -9,8 +9,18 @@ public class Ejercicio10App {
 	public static void main(String[] args) {
 		int tamaño = Integer.parseInt(JOptionPane.showInputDialog("Introduce el tamaño de la array"));
 		int array[] = new int[tamaño];
+
+		int num1 = Integer
+				.parseInt(JOptionPane.showInputDialog("Introduce el primer valor para el rango de números aleatorios"));
+		int num2 = Integer.parseInt(
+				JOptionPane.showInputDialog("Introduce el segundo valor para el rango de números aleatorios"));
+
+		rellenarArray(array, Math.min(num1, num2), Math.max(num1, num2));
+		mostrarArray(array);
+		JOptionPane.showMessageDialog(null, "El valor máximo de la array es " + maxArray(array));
 	}
 
+	// Dice si un entero es primo o no
 	public static boolean esPrimo(int num) {
 		if (num <= 1) {
 			return false;
@@ -28,6 +38,7 @@ public class Ejercicio10App {
 
 	}
 
+	// Genera un número primo aleatorio entre un rango dado.
 	public static int nrandom(int start, int end) {
 		Random r = new Random();
 		int num = r.nextInt(end - start + 1) + start;
@@ -37,10 +48,31 @@ public class Ejercicio10App {
 		return num;
 
 	}
-	
+
+	// Rellena una array con enteros primos aleatorios
 	public static void rellenarArray(int[] a, int min, int max) {
 		for (int i = 0; i < a.length; i++) {
 			a[i] = nrandom(min, max);
+		}
+	}
+
+	// recorre una array y devuelve el valor máximo de la misma.
+	public static int maxArray(int[] a) {
+		int max = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+
+		return max;
+	}
+
+	// Imprime por consola el contenido de una array
+	public static void mostrarArray(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Valor de la array en la posición " + i + ": " + a[i]);
 		}
 	}
 }
