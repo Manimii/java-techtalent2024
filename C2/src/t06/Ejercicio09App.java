de46ad2ce4a1997b1baf067578e1ef9f3b64c0f4
Manel Nin <mninminet@gmail.com>
2024-02-21 13:37:24 +0100

    Update Ejercicio09App.java

--- C2/src/t06/Ejercicio09App.java
+++ C2/src/t06/Ejercicio09App.java
@@ -8,7 +8,7 @@ public class Ejercicio09App {
 
 		int tamaño = Integer.parseInt(JOptionPane.showInputDialog("Introduce el tamaño de la array"));
 		int array[] = new int[tamaño];
-		rellenarArray(array);
+		rellenarArrayRango(array, 5, 15);
 		mostrarArray(array);
 	}
 
@@ -17,14 +17,20 @@ public class Ejercicio09App {
 			a[i] = (int) (Math.random() * 10);
 		}
 	}
-	
+
+	public static void rellenarArrayRango(int[] a, int min, int max) {
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int) (min + (Math.random() * (max + 1 - min)));
+		}
+	}
+
 	public static void mostrarArray(int[] a) {
 		int suma = 0;
 		for (int i = 0; i < a.length; i++) {
 			System.out.println("Valor de la array en la posición " + i + ": " + a[i]);
 			suma += a[i];
 		}
-		
+
 		System.out.println("Suma de los valores de la array = " + suma);
 	}
 }
