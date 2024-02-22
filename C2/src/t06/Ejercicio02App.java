a17e33e4793a6f2b4e75b87b3a6b1ca16e8c7814
Manel Nin <mninminet@gmail.com>
2024-02-22 10:39:27 +0100

    Update Ejercicio02App.java

--- C2/src/t06/Ejercicio02App.java
+++ C2/src/t06/Ejercicio02App.java
@@ -7,19 +7,19 @@ import javax.swing.JOptionPane;
 public class Ejercicio02App {
 
 	public static void main(String[] args) {
-		int n = Integer
-				.parseInt(JOptionPane.showInputDialog("Introduce el número de números aleatorios que quieres generar"));
+		int n = Integer.parseInt(
+				JOptionPane.showInputDialog("Introduce la cantidad de números aleatorios que quieres generar"));
 
 		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Rango mínimo de la generación aleatoria"));
 		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Rango máximo de la generación aleatoria"));
 
 		for (int i = 0; i < n; i++) {
-			System.out.println((i + 1) + ": " + nrandom(n1, n2));
+			System.out.println((i + 1) + ": " + nrandom(Math.min(n1, n2), Math.max(n1, n2)));
 		}
 	}
 
-	public static int nrandom(int start, int end) {
-		Random r = new Random();
-		return r.nextInt(end - start + 1) + start;
+	public static int nrandom(int min, int max) {
+		return (int) (min + (Math.random() * (max + 1 - min)));
+
 	}
 }
