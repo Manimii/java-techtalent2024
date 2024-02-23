724d01a162b95d8870a0cf3ae2c2b1bb9d1f0722
Manel Nin <mninminet@gmail.com>
2024-02-23 09:34:24 +0100

    Update Ejercicio06App.java

--- C2/src/t06/Ejercicio06App.java
+++ C2/src/t06/Ejercicio06App.java
@@ -5,10 +5,10 @@ import javax.swing.JOptionPane;
 public class Ejercicio06App {
 
 	public static void main(String[] args) {
-		int numero = 0;
-		do {
-			numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número entero positivo"));
-		} while (numero < 0);
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número entero positivo"));
+		while (numero < 0) {
+			numero = Integer.parseInt(JOptionPane.showInputDialog("Error: Debes introducir un número entero positivo"));
+		}
 
 		JOptionPane.showMessageDialog(null, "El número " + numero + " tiene " + cuentaCifras(numero) + " cifra(s)");
 
