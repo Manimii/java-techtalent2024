390e7857a77eafe087849f6b7cc08551e458d2a9
Manel Nin <mninminet@gmail.com>
2024-02-27 10:46:38 +0100

    Update Ejercicio04App.java

--- C2/src/t06/extra/Ejercicio04App.java
+++ C2/src/t06/extra/Ejercicio04App.java
@@ -1,16 +1,58 @@
 package t06.extra;
 
+import java.util.Arrays;
+
 public class Ejercicio04App {
 
+	/*
+	 * Eliminar Duplicados: Crea un programa que elimine los elementos duplicados de
+	 * un array de números enteros, es decir, si un número aparece más de una vez en
+	 * el array, debería aparecer solo una vez en el array resultante.
+	 */
+
 	public static void main(String[] args) {
-		int[] numeros = {1,2,3,4,5,6,1,2,1,10,11};
-		
+		int[] numeros = { 1, 2, 3, 4, 5, 6, 1, 2, 1, 10, 11 };
+		muestraArray(numeros);
+		eliminaDuplicados(numeros);
+
 	}
-	
-	public static void eliminaDuplicados() {
+
+	public static void eliminaDuplicados(int[] a) {
+		Arrays.sort(a);
+		muestraArray(a);
+
+		int count = 0;
+		int actual = -1;
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == actual) {
+				count++;
+			}
+			actual = a[i];
+		}
+		actual = -1;
+
+		int[] b = new int[a.length - count];
+
+		int i = 0;
+		while (i < b.length) {
+			for (int j = 0; j < a.length; j++) {
+				if (a[j] == actual) {
+					j++;
+				} else {
+					b[i] = a[j];
+					muestraArray(b);
+					i++;
+				}
+				actual = a[j];
+				
+				System.out.println(j);
+			}
+		}
 		
+		muestraArray(b);
 	}
-	
+
 	public static void muestraArray(int[] a) {
 		System.out.print("[");
 		for (int i = 0; i < a.length; i++) {
