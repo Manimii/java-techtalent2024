2e19c3b282a52d9edb976ebf9111f05a8ce2de78
Manel Nin <55589406+Manimii@users.noreply.github.com>
2024-06-24 20:56:00 +0200

    Ejercicios Bootstrap

---
+++ C5/t29/css/Ejercicio08.css
@@ -0,0 +1,30 @@
+#calculadora {
+    width: 263px;
+    height: 336px;
+}
+
+#display {
+    height: 84px;
+}
+
+#cuerpo {
+    height: 252px;
+}
+
+.numeros{
+    background-color: #8D6E63;
+    height: 63px;
+}
+
+.numeros:hover {
+    background-color: #6b534a;
+}
+
+.operadores {
+    background-color: #4DB7AD;
+    height: 42px;
+}
+
+.operadores:hover {
+    background-color: #3a867f;
+}
