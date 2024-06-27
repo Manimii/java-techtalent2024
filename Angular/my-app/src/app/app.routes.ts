a8ca066a5ebaca6948a4c5a0fdfe77ccb47ad42f
Manel Nin <mninminet@gmail.com>
2024-06-27 10:41:48 +0200

    Angular

--- Angular/my-app/src/app/app.routes.ts
+++ Angular/my-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'portfolio', component: PortfolioComponent},
+    {path: '**', component: PageNotFoundComponent},
     {path: '', redirectTo: 'portfolio', pathMatch: 'full'},
 ];
--- Angular/my-app/src/app/shared/footer/footer.component.ts
+++ Angular/my-app/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
-}
+  year = new Date().getFullYear();
+};
