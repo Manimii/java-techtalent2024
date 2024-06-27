6877198b8fe1c42cc27c005ea94b8c7b9f30741b
Manel Nin <mninminet@gmail.com>
2024-06-27 13:48:06 +0200

    UD28

--- Angular/my-app/src/app/pages/portfolio/portfolio.component.ts
+++ Angular/my-app/src/app/pages/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
